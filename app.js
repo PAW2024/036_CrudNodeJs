10ae6968d1113a4aeaa4c38554a8c32505699b45
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-25 07:06:20 +0700

    POST Method (Menambah Data)

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const todoroutes = require('./routes/todo.js');
 
+
+app.use(express.json());
+app.use('/todos',todoroutes);
 app.set('view engine', 'ejs');
-
 app.get('/', (req, res) =>{
     res.render('index');
 });

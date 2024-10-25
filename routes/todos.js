ca08e64d454f473f1697193e5bdfb305c78fd4ba
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-25 08:26:04 +0700

    CRUD_RESTAPI

--- routes/todos.js
+++ routes/todos.js
@@ -33,5 +33,18 @@ router.delete('/:id', (req, res) => {
 
 });
 
+router.put('/:id', (req, res) => {
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) return res.status(404).json ({message: 'Tugas tidak ditemukan'});
+    todo.task = req.body.task || todo.task;
+
+    res.status(200).json({
+        message: `Tugas dengan ID ${todo.id} telah diperbaharui`,
+        updateTodo: todo
+    });
+
+
+
+});
 
 module.exports = router;
